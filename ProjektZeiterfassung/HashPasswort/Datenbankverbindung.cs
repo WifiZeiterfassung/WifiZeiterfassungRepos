a8ab3b0312fd449d974180ae991e8450e9dff554
Josef <digitalman@a1.net>
2017-05-23 21:18:38 +0200

    HashWert eintrsagen mit Konsolenapp

---
+++ ProjektZeiterfassung/HashPasswort/Datenbankverbindung.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPasswort
+{
+    class Datenbankverbindung
+    {
+        /// <summary>
+        /// Internes Hilfsfeld
+        /// </summary>
+        private string _DbConnection = String.Empty;
+        /// <summary>
+        /// Stellt eine Eigenschaft für die DBConnection bereit
+        /// </summary>
+        public string DbConnection
+        {
+            get
+            {
+                if (_DbConnection == String.Empty || _DbConnection == null )
+                {
+                    _DbConnection = Con();
+                }
+                return _DbConnection;
+            }
+            set { _DbConnection = value; }
+        }
+
+        public Datenbankverbindung()
+        {
+            DbConnection = _DbConnection;
+        }
+        /// <summary>
+        /// Methode welche den Connection String zur Datenbankverbindung herrstellt
+        /// </summary>
+        /// <returns></returns>
+        private string Con()
+        {
+            //var ConBauer = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            //ConBauer.DataSource = this.SqlServerAdresse;
+            //ConBauer.InitialCatalog = Datenbankname;
+            //ConBauer.IntegratedSecurity = true;
+            //this._DbConnection = ConBauer.ConnectionString;
+            this._DbConnection = HashPasswort.Properties.Settings.Default.ZEIT2017ConnectionString;
+            return _DbConnection;
+        }
+    }
+}
---
+++ ProjektZeiterfassung/HashPasswort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashPasswort;
+
+namespace HashPasswort
+{    
+    class Program
+    {
+        private string _Klartextpasswort;
+        private byte[] _Passwort;
+        private int _ID;
+        private string _Connection;
+        private string _SqlString = "";//Hier den SQL String fürs Update
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.Write("Id des Mitarbeiters eingeben :");
+            p._ID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte Klartextpasswort eingeben :");
+            p._Klartextpasswort = Console.ReadLine();
+            p._Passwort = Helper.GetHash(p._Klartextpasswort);
+
+            Datenbankverbindung con = new Datenbankverbindung();
+            using (var Connection = new System.Data.SqlClient.SqlConnection(con.DbConnection))
+            {
+                using(var Befehl = new System.Data.SqlClient.SqlCommand("dbo.Mitarbeiter", Connection))
+                {
+                    Befehl.CommandType = System.Data.CommandType.TableDirect;
+                    Befehl.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = p._ID;
+                    Befehl.Parameters.Add("@Passwort", System.Data.SqlDbType.VarBinary).Value = p._Passwort;
+
+                }
+            }
+            
+
+
+        }
+    }
+}
