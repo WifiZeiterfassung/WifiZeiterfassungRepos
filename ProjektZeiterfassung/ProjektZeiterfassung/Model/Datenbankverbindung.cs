e67f66f57b8c5df3501adfd7da0b1b35a0b4e8fb
digitalman <digitalman@a1.net>
2017-06-06 18:14:12 +0200

    Sql strings

--- ProjektZeiterfassung/ProjektZeiterfassung/Model/Datenbankverbindung.cs
+++ ProjektZeiterfassung/ProjektZeiterfassung/Model/Datenbankverbindung.cs
@@ -12,6 +12,15 @@ namespace ProjektZeiterfassung.Model
     public class Datenbankverbindung
     {
 
+        //SQL String fürs Update des Passwortes
+        private string _SqlString = "UPDATE [ZEIT2017].[dbo].[Mitarbeiter] SET [Passwort] = @Passwort WHERE [ID] = @ID;";
+        //Insert statement für Mitarbeiter in der Datenbank
+        private string _sqlStringInsertMitarbeiter = "INSERT INTO [ZEIT2017].[dbo].[Mitarbeiter](Vorname,Nachname,Passwort) " +
+                                                                                        "VALUES (@Vorname,@Nachname,@Passwort);";
+        //Insert für Eintrittaustritt tabelle in der Datenbank
+        private string _sqlStringInsertEintrittAustritt = "INSERT INTO [ZEIT2017].[dbo].[EintrittAustritt](FK_Mitarbeiter,Personalnummer,EintrittsDatum,TagesSollZeit,IsAdmin) " +
+                                                                                                  "VALUES (@ID,@PerNr,@EDatum,@SollZeit,@IsAdmin);";
+
         /// <summary>
         /// Internes Hilfsfeld
         /// </summary>
@@ -54,6 +63,66 @@ namespace ProjektZeiterfassung.Model
             //this._DbConnection = Properties.Settings.Default.ZEIT2017ConnServer; //SqlServerAdresse-SERVER 192.168.1.103
             this._DbConnection = Properties.Settings.Default.ZEIT2017ConnectionString; //In der wifi diese zuweisung verwenden .\SQLEXPRESS
             return _DbConnection;
+        }
+
+        /// <summary>
+        /// Methode ändert das Passwort des Mitarbeiters
+        /// </summary>
+        /// <param name="id">Id des Mitarbeiters</param>
+        /// <param name="pwd">Hashwert des Klartextpassworts</param>
+        private void PasswortAendern(int id, byte[] pwd)
+        {
+            //Später wenn alles in Ortnung einen TRy Catch und finally block einfügen
+            using (var Connection = new System.Data.SqlClient.SqlConnection(this._DbConnection))
+            {
+                Connection.Open();
+                using (var Befehl = new System.Data.SqlClient.SqlCommand("dbo.Mitarbeiter", Connection))
+                {
+                    Befehl.CommandText = this._SqlString;
+                    Befehl.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = id;
+                    Befehl.Parameters.Add("@Passwort", System.Data.SqlDbType.VarBinary).Value = pwd;
+                    Befehl.ExecuteNonQuery();
+                }
+                Connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Methode speichert einen neuen Mitarbeiter in die Datenbank nur für Admin. 
+        /// Dafür müssen Daten in zwei Tabellen geschrieben werden dbo.Mitarbeiter und dbo.EintrittAustritt
+        /// </summary>
+        /// <param name="m">Mitarbeiter Objekt</param>
+        /// <param name="ea">EintrittAustritt Objekt</param>
+        private void NeuenMitarbeiterSpeichern(Mitarbeiter m, EintrittAustritt ea)
+        {
+            //Später wenn alles in Ortnung und ohne Fehler läuft mit einen Try Catch und finally block absichern
+            //Methode vielleicht noch ändern damit ein Rückgabewert retourniert wird 
+            using (var Connection = new System.Data.SqlClient.SqlConnection(this._DbConnection))
+            {
+                Connection.Open();
+                //Tabelle Mitarbeiter mit Vorname Nachnam und Passwort das übergeben wird befüllen 
+                using (var Befehl = new System.Data.SqlClient.SqlCommand("dbo.Mitarbeiter", Connection))
+                {
+                    Befehl.CommandText = this._sqlStringInsertMitarbeiter;
+                    Befehl.Parameters.Add("@Vorname", System.Data.SqlDbType.NVarChar).Value = m.Vorname;
+                    Befehl.Parameters.Add("@Nachname", System.Data.SqlDbType.NVarChar).Value = m.Nachname;
+                    Befehl.Parameters.Add("@Passwort", System.Data.SqlDbType.VarBinary).Value = m.Passwort;
+                    Befehl.ExecuteNonQuery();
+                }
+
+                //Tabelle EintrittAustritt mit Id vom Mitarbeiter,Personalnummer,EintrittsDatum,SollZeit und IsAdmin befüllen
+                using (var Befehl = new System.Data.SqlClient.SqlCommand("dbo.EintrittAustritt", Connection))
+                {
+                    Befehl.CommandText = this._sqlStringInsertEintrittAustritt;
+                    Befehl.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = ea.FK_Mitarbeiter;
+                    Befehl.Parameters.Add("@PerNr", System.Data.SqlDbType.NVarChar).Value = ea.Personalnummer;
+                    Befehl.Parameters.Add("@EDatum", System.Data.SqlDbType.DateTime).Value = ea.EintrittsDatum;
+                    Befehl.Parameters.Add("@SollZeit", System.Data.SqlDbType.Decimal).Value = ea.TagesSollZeit;
+                    Befehl.Parameters.Add("@IsAdmin", System.Data.SqlDbType.Bit).Value = ea.IsAdmin;
+                    Befehl.ExecuteNonQuery();
+                }
+                Connection.Close();
+            }
         }
     }
 }
